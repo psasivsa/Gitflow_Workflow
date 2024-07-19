4219cfc226e424f654e75c7c68a6266e79de435b
jose benito lopez <josebenito.lopez@external.stellantis.com>
2024-07-19 11:42:03 +0200

    Bump version to 2.5.1

--- search.js
+++ search.js
@@ -4,3 +4,7 @@ let position = text.search("Blue");
 let str1 = "GeeksforGeeks, A computer science portal";
 let result = str1.match(/computer/g);
 console.log(result);
+
+
+const hotfix1 = "Bump version to 2.5.1"
+console.log(hotfix1)
